ffab3acc46faed1d917afa5959c5ad4703e1a697
xpanvictor <xpanvictor@gmail.com>
2022-06-18 15:23:53 +0100

    First: Event n streams

---
+++ event1.js
@@ -0,0 +1,14 @@
+const Readable = require('stream').Readable;
+
+let r = new Readable();
+
+let count = 0;
+r._read = function () {
+  count++;
+  if (count > 10) {
+    return r.push(null);
+  }
+  setTimeout(() => r.push(count + '\n'), 500);
+};
+
+r.pipe(process.stdout);
