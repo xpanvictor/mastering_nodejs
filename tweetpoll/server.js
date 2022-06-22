831f4fbe9dd475677390434f02d3ebc606c41325
xpanvictor <xpanvictor@gmail.com>
2022-06-22 16:06:19 +0100

    tweetpoll

--- tweetpoll/server.js
+++ tweetpoll/server.js
@@ -1,9 +1,38 @@
-let fs = require('fs');
 let http = require('http');
+let fs = require('fs');
+let cryp = require('crypto');
 
 let theuser = null;
 let userpos = 0;
 let tweetfile = 'tweets.txt';
+
+let sendNext = function (fd) {
+  let buffer = Buffer.alloc(140);
+  fs.read(fd, buffer, 0, 140, userpos * 140, (err, num) => {
+    if (!err && num > 0 && theuser) {
+      ++userpos;
+      theuser.write(`data: ${buffer.toString('utf-8', 0, num)}\n\n`);
+      return process.nextTick(() => {
+        sendNext(fd);
+      });
+    }
+  });
+};
+
+function start() {
+  fs.open(tweetfile, 'r', (err, fd) => {
+    if (err) {
+      setTimeout(start, 1000);
+    }
+    fs.watch(tweetfile, (event, filename) => {
+      if (event === 'change') {
+        sendNext(fd);
+      }
+    });
+  });
+}
+
+start();
 
 http
   .createServer((req, res) => {
